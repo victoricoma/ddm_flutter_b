1b8bbeabef571659a98bc59b727eb63c94371a48
victoricoma <victor.icoma@etec.sp.gov.br>
2024-02-27 18:10:13 -0300

    Aula2702 Classe Principal.

---
+++ perguntas/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Ola Flutter!!'),
+    );
+  }
+}
