cbe883adae284c27d961dc45224b225784a19d54
victoricoma <victor.icoma@etec.sp.gov.br>
2024-04-02 18:16:32 -0300

    Aula0204 Funções com Flutter

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -5,6 +5,12 @@ void main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+  void responder() {
+    print('Pergunta Respondida!');
+  }
+
+  @override
   Widget build(BuildContext context) {
     final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
@@ -13,11 +19,11 @@ class PerguntaApp extends StatelessWidget {
         title: const Text('Perguntas'),
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           Text(perguntas[0]),
-          ElevatedButton(onPressed: null, child: const Text('Resposta 1')),
+          ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
           Text(perguntas[1]),
-          ElevatedButton(onPressed: null, child: const Text('Resposta 2'))
+          ElevatedButton(onPressed: responder, child: Text('Resposta 2'))
         ],
       ),
     ));
