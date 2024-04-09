6b93ed4f515ba5fad7d8a40ce7ed9d9b06bc930b
RyanAlexandre23 <ryan.conceicao4@etec.sp.gov.br>
2024-04-09 18:02:40 -0300

    Aula0904 Stateful Stateless

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -4,28 +4,46 @@ void main() {
   runApp(new PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({super.key});
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
   void responder() {
-    print('Pergunta Respondida!');
+    setState(() {
+      perguntaSelecionada++;
+      if (perguntaSelecionada > 2) {
+        perguntaSelecionada = 0;
+      }
+    });
+    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
+    final perguntas = [
+      'Qual sua cor favorita?',
+      'Qual seu animal favorito?',
+      'Qual sua comida favorita?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: const Text('Perguntas'),
       ),
       body: Column(
-        children: <Widget>[
-          Text(perguntas[0]),
-          ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
-          Text(perguntas[1]),
-          ElevatedButton(onPressed: responder, child: Text('Resposta 2'))
+        children: [
+          Text(perguntas[perguntaSelecionada]),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
+          ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
         ],
       ),
     ));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
