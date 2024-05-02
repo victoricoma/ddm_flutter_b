178638b9a452b55df55976c8a249c5a95ae23627
victoricoma-soulcode <victor.icoma@soulcode.com>
2024-05-02 14:06:31 -0300

    Aula2504 Função de Call Back

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
-void main() {
-  runApp(new PerguntaApp());
-}
+void main() => runApp(const PerguntaApp());
 
 class PerguntaAppState extends State<PerguntaApp> {
   var perguntaSelecionada = 0;
+
   void responder() {
     setState(() {
       perguntaSelecionada++;
     });
-    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Perguntas'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Questao(perguntas[perguntaSelecionada]),
+            Resposta('Resposta 1', responder),
+            Resposta('Resposta 2', responder),
+            Resposta('Resposta 3', responder),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Questao(perguntas[perguntaSelecionada]),
-          ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
-          ElevatedButton(onPressed: responder, child: const Text('Resposta 2')),
-          ElevatedButton(onPressed: responder, child: const Text('Resposta 3'))
-        ],
-      ),
-    ));
+    );
   }
 }
 
 class PerguntaApp extends StatefulWidget {
   const PerguntaApp({super.key});
+
   @override
   PerguntaAppState createState() {
     return PerguntaAppState();
