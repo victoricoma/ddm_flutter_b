96b1e71d81b0cc83e265c8a41485d01ab2bad232
victoricoma <victor.icoma@etec.sp.gov.br>
2024-05-07 18:17:27 -0300

    Aula0705 - Ajustes de Organização de Vetores de Objetos

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -16,8 +16,31 @@ class PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
     final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      },
+      {
+        'texto': 'Qual é o seu time favorito?',
+        'respostas': ['São Paulo', 'Corinthians', 'Santos', 'Palmeiras']
+      },
+      {
+        'texto': 'Qual é o seu deus do rock favorito?',
+        'respostas': [
+          'Fred Mercury',
+          'Elvis Presley',
+          'Kurt Cobain',
+          'Jhon Lenon'
+        ]
+      },
+      {
+        'texto': 'Qual é a sua universidade favorita?',
+        'respostas': ['USP', 'UNESP', 'Unicamp', 'UFSCar']
+      },
     ];
 
     return MaterialApp(
@@ -27,7 +50,7 @@ class PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Questao(perguntas[perguntaSelecionada]),
+            Questao(perguntas[perguntaSelecionada]['texto'] as String),
             Resposta('Resposta 1', responder),
             Resposta('Resposta 2', responder),
             Resposta('Resposta 3', responder),
