e163cde6a81fb51d9a7cf3bc2f1d5bfb703e67d8
victoricoma <victor.icoma@etec.sp.gov.br>
2024-05-28 18:50:57 -0300

    Aula2805

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -1,45 +1,67 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
 import './resultado.dart';
 
 void main() => runApp(const PerguntaApp());
 
 class PerguntaAppState extends State<PerguntaApp> {
   var perguntaSelecionada = 0;
+  var pontuacaoTotal = 0;
   final perguntas = [
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu time favorito?',
-      'respostas': ['São Paulo', 'Corinthians', 'Santos', 'Palmeiras']
+      'respostas': [
+        {'texto': 'São Paulo', 'pontuacao': 10},
+        {'texto': 'Corinthians', 'pontuacao': 5},
+        {'texto': 'Santos', 'pontuacao': 3},
+        {'texto': 'Palmeiras', 'pontuacao': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu deus do rock favorito?',
       'respostas': [
-        'Fred Mercury',
-        'Elvis Presley',
-        'Kurt Cobain',
-        'Jhon Lenon'
-      ]
+        {'texto': 'Fred Mercury', 'pontuacao': 10},
+        {'texto': 'Elvis Presley', 'pontuacao': 5},
+        {'texto': 'Kurt Cobain', 'pontuacao': 3},
+        {'texto': 'Jhon Lenon', 'pontuacao': 1},
+      ],
     },
     {
       'texto': 'Qual é a sua universidade favorita?',
-      'respostas': ['USP', 'UNESP', 'Unicamp', 'UFSCar']
+      'respostas': [
+        {'texto': 'USP', 'pontuacao': 10},
+        {'texto': 'UNESP', 'pontuacao': 5},
+        {'texto': 'Unicamp', 'pontuacao': 3},
+        {'texto': 'UFSCar', 'pontuacao': 1},
+      ],
     },
   ];
-  void responder() {
+  void responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
         perguntaSelecionada++;
+        pontuacaoTotal += pontuacao;
       });
     }
+    print(pontuacaoTotal);
   }
 
   bool get temPerguntaSelecionada {
@@ -48,23 +70,18 @@ class PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? perguntas[perguntaSelecionada]['respostas'] as List<String>
-        : [];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(
-                children: [
-                  Questao(perguntas[perguntaSelecionada]['texto'] as String),
-                  ...respostas.map((t) => Resposta(t, responder)).toList()
-                ],
+            ? Questionario(
+                perguntas: perguntas,
+                perguntaSelecionada: perguntaSelecionada,
+                quandoResponder: responder,
               )
-            : const Resultado(),
+            :  const Resultado(),
       ),
     );
   }
--- perguntas/lib/resultado.dart
+++ perguntas/lib/resultado.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({super.key});
+  const Resultado({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-        'Parabéns',
+        "Parabéns",
         style: TextStyle(
           fontSize: 28,
         ),
